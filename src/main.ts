c6d1230a255e145c8f690491ea8d7d9f7b60d687
Nishan <nishan.bhattrai@aitcentre.com>
2025-04-11 14:01:16 +0545

    feat:build error fixed

--- src/main.ts
+++ src/main.ts
@@ -8,9 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.enableCors({
-    origin:
-      'http://localhost:5173/' ||
-      'https://finance-management-system-nine.vercel.app/', // Change this to your frontend URL if needed
+    origin: 'http://localhost:5173/', // Change this to your frontend URL if needed
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
