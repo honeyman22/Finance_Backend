cb2d18c57a3034a79926cfdaf3969d0621574a13
Nishan <nishan.bhattrai@aitcentre.com>
2025-05-09 14:23:39 +0545

    feat: user section added done

--- src/admin/user/dtos/create-user.dtos.ts
+++ src/admin/user/dtos/create-user.dtos.ts
@@ -19,18 +19,6 @@ export const createUserSchema = z
       .string({ required_error: 'Phone number is required' })
       .trim(),
     activationDate: z.string({ required_error: 'Activation date is required' }),
-    password: z
-      .string({ required_error: 'Password is required' })
-      .trim()
-      .min(8, 'Password must be at least 8 characters long')
-      .max(64, 'Password must be less than 64 characters')
-      .regex(/[A-Z]/, 'Password must contain at least one uppercase letter')
-      .regex(/[a-z]/, 'Password must contain at least one lowercase letter')
-      .regex(/\d/, 'Password must contain at least one number')
-      .regex(
-        /[^A-Za-z0-9]/,
-        'Password must contain at least one special character',
-      ),
   })
   .strict();
 export class CreateUserDto extends createZodDto(createUserSchema) {}
